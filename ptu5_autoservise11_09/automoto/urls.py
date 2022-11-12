c90756420b44d2627fe15b6a59651a2da3d4321f
Darius Vasionis <darius@vasionis.eu>
2022-11-12 14:52:01 +0200

    Pakeisti laukai

--- ptu5_autoservise11_09/automoto/urls.py
+++ ptu5_autoservise11_09/automoto/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('car/', views.cars, name='cars'),
+    path('car/<int:pk>/', views.cars, name='car'),
+    path('orders/', views.orders, name="order")
 ]
--- ptu5_autoservise11_09/automoto/views.py
+++ ptu5_autoservise11_09/automoto/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.core.paginator import Paginator
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
-from . models import Car, Service, OrderLine
+from . models import Car, Service, OrderLine, CarModel, Order
 
 # Create your views here.
 def index(request):
@@ -14,3 +17,16 @@ def index(request):
     }
 
     return render(request,'automoto/index.html', context)
+
+def cars(request):
+    return render(request, 'automoto/cars.html', {'cars': Car.objects.all()})
+
+def car_info(request, car_id):
+    return render(request, 'automoto/car_info.html', {'cars': get_object_or_404(Car,id=car_id)})
+
+def orders(request):
+    return render(request, 'automoto/orders.html', {'orders':Order.objects.all()})
+
+class OrderLineView(ListView):
+    model = Order
+    template_name = 'automoto/order_line.html'
